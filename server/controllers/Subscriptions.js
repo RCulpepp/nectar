ebd1a7ae36c121420eefc0b215ed2fb5b66c4cdd
Ryan Culpepper <ryan.culpepper@gmail.com>
2016-07-29 10:43:16 -0500

    Added Ignored files

--- server/controllers/Subscriptions.js
+++ server/controllers/Subscriptions.js
@@ -3,7 +3,7 @@ var Subscription = mongoose.model('Subscription');
 var Order = mongoose.model('Order');
 var crypto = require('crypto');
 function Subscriptions(){
-
+	
 }
 
 module.exports = new Subscriptions;
